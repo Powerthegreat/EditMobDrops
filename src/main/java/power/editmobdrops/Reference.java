a8449e07df3c5b91abed5d5f32142e3b026d3d5d
Power <powerfinnthegreat@gmail.com>
2021-02-13 07:51:59 +1100

    Update 1.3.3

--- src/main/java/power/editmobdrops/Reference.java
+++ src/main/java/power/editmobdrops/Reference.java
@@ -2,6 +2,6 @@ package power.editmobdrops;
 
 public class Reference {
 	public static final String MODID = "editmobdrops";
-	public static final String VERSION = "1.3.2";
+	public static final String VERSION = "1.3.3";
 	public static String CONFIG_PATH = "/editmobdrops/";
 }
